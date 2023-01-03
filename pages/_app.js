ad2b0b403538929f484cb1910ce21ff101ce898c
Thomas <114309234+thomasweigert91@users.noreply.github.com>
2023-01-03 09:51:57 +0100

    fixed deploy test

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,17 @@
 import GlobalStyles from "../styles/GlobalStyles";
 import Head from "next/head";
 import {createContext} from "react";
-import {useState} from "react";
 
 export const stateContext = createContext();
 
 function MyApp({Component, pageProps}) {
-  const [lastWeight, setLastWeight] = useState("");
-  const [currentWeight, setCurrentWeight] = useState("");
-  const [reps, setReps] = useState("");
-
   return (
     <>
       <Head>
         <title>{Component.title}</title>
       </Head>
       <GlobalStyles />
-      <Component
-        {...pageProps}
-        currentWeight={currentWeight}
-        reps={reps}
-        lastWeight={lastWeight}
-      />
+      <Component {...pageProps} />
     </>
   );
 }
